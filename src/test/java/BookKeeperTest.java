783a1592757fbe8cc362fea9ca2c67f15dbc0d17
Bartosz Suśniło <susnilox@gmail.com>
2018-03-27 18:52:44 +0200

    Change setup

--- src/test/java/BookKeeperTest.java
+++ src/test/java/BookKeeperTest.java
@@ -4,29 +4,49 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
+import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
 import pl.com.bottega.ecommerce.sales.domain.invoicing.*;
 import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductData;
+import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductType;
+import pl.com.bottega.ecommerce.sharedkernel.Money;
+
+import java.util.Date;
 
 import static org.junit.Assert.*;
+import static pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductType.FOOD;
+import static pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductType.STANDARD;
+
 @RunWith(MockitoJUnitRunner.class)
 public class BookKeeperTest {
     BookKeeper bookKeeper;
     private InvoiceFactory invoiceFactory;
+    @Mock
     private TaxPolicy taxPolicy;
     private InvoiceRequest invoiceRequest;
     @Mock
     private ClientData client;
+    private Money money;
     @Mock
     InvoiceLine invoiceLine;
+    ProductData productData;
+    ProductType type;
     @Before
     public void setUp() {
         bookKeeper = new BookKeeper(invoiceFactory);
         invoiceFactory= new InvoiceFactory();
+        invoiceRequest = new InvoiceRequest(new ClientData(Id.generate(),"Blab sa"));
+        money = new Money(12);
+        Date snapshotDate = new Date();
+        productData = new ProductData(Id.generate(), money, FOOD, snapshotDate);// Money price, String name, ProductType type, Date snapshotDate
+
+
     }
     @Test
     public void oneInvoiceTest(){
         bookKeeper = new BookKeeper(invoiceFactory);
-        Invoice invoice = invoiceFactory.create(client);
+        Invoice invoice = bookKeeper.issuance(invoiceRequest,taxPolicy);
+
+        //Invoice invoice = invoiceFactory.create(client);
         invoice.addItem(invoiceLine);
         invoice.getItems();
         invoice.getItems().size();
